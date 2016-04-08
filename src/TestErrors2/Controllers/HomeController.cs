106acbc4e3fa4b0fc88a8dae8cc0ea6544bfffd7
Alex Mitchell <Alex.Mitchell@labor.idaho.gov>
2016-04-08 14:12:53 -0600

    Initial commit

---
+++ src/TestErrors2/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace TestErrors2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content(HttpContext.Request.PathBase);
+        }
+        
+        public IActionResult Error()
+        {
+            throw new Exception();
+        }
+    }
+}
